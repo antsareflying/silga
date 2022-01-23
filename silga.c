b41a104a984946c0c43ba66d25715ddbb39b1c47
antsareflying <seongmumoon@gmail.com>
2022-01-23 17:42:38 +0800

    got it working except the fitness is wrong?

--- silga.c
+++ silga.c
@@ -3,19 +3,19 @@
 #include <time.h>
 #include <math.h>
 #include <stdbool.h>
+#include <math.h>
 #include "include/sodium.h"
 
-double double_rand_range(double a, double b);
-int int_rand_range(int a, int b);
 int cmpfunc (const void * a, const void * b);
 bool check_fitness_zero(double populationfitness[30]);
 
 
 int generation = 0;
-double population[30][6] = {0};
-double populationfitness[30] = {1};
-double new_population[30][6] = {0};
-
+int population[30][6] = {0};
+int new_population[30][6] = {0};
+double populationfitness[30] = {1000000};
+int parent_1[30] = {100};
+int parent_2[30] = {100};
 
 int main(void)
 {
@@ -25,55 +25,65 @@ int main(void)
 		return 1;
 	}
 
-	int generation = 0;
-	double population[30][6] = {0};
-	double populationfitness[30] = {1};
-	int parent_1[30] = {100};
-	int parent_2[30] = {100};
-
-	srand(time(NULL));
 
 	for(int i = 0; i < 30; i++)
 	{
 		for( int j = 0; j < 6; j++)
 		{
-			population[i][j] = randombytes_uniform(10000) - 5000; /*random number b/w -5000 and 5000*/
+			population[i][j] = (randombytes_uniform(100)-50); /*random number b/w -50 and 50*/
 		}
 	}
-	
+
 	
 	for(int i = 0; i < 30; i++)
 	{
-		populationfitness[i] = (population[i][1]/*a*/ + 30 * pow(population[i][2], 2) * population[i][3]/*30b^2c*/ - 2 * pow(population[i][4], 3) * pow(population[i][5], 2)/*2d^3e^2*/ + 2 * population[i][6]/*2f*/ - 2567);
-		/* calculate fitness function (a+30b^2c-2d^3e^2+2f) - 2567*/
+		populationfitness[i] = (double)(population[i][0]/*a*/ + 30 * population[i][1] * population[i][2]/*30bc*/ - (2 * population[i][3]) * population[i][4] /*2de*/ + 2 * population[i][5]/*2f*/ - 30);
+		/* calculate fitness function (a+30bc-2de+2f) - 30*/
 	}
 
-	while (check_fitness_zero(populationfitness) == false)
+	bool soln_found = false;
+	double best_fitness = 10000000000;
+	int best_fitness_index = 31;
+	int best_fitness_index_counter = 0;
+	int best_fitness_soln[6] = {0};
+
+	while (soln_found == false)
 	{
 		//calc fitness function
 		for(int i = 0; i < 30; i++)
 		{
-			populationfitness[i] = (population[i][1]/*a*/ + 30 * pow(population[i][2], 2) * population[i][3]/*30b^2c*/ - 2 * pow(population[i][4], 3) * pow(population[i][5], 2)/*2d^3e^2*/ + 2 * population[i][6]/*2f*/ - 2567);
-			/* calculate fitness function (a+30b^2c-2d^3e^2+2f) - 2567*/
-		}
-
-		//check if soln found
-		for(int i = 0; i < 30; i++)
-		{
-			if (populationfitness[i] == 0)
+			populationfitness[i] = (double)(population[i][0]/*a*/ + 30 * population[i][1] * population[i][2]/*30bc*/ - (2 * population[i][3]) * population[i][4] /*2de*/ + 2 * population[i][5]/*2f*/ - 30);
+			/* calculate fitness function (a+30bc-2de+2f) - 30*/
+			
+			printf("Fitness gen %d row %d: %lf\n", generation, i, populationfitness[i]);
+			if(fabs(populationfitness[i]) < fabs(best_fitness))
 			{
-				return 0;
+				best_fitness = populationfitness[i];
+				best_fitness_index = i;
+				for(int j = 0; j< 6; j++)
+				{
+					best_fitness_soln[j] = population[i][j];
+				}
+			}
+			else if(populationfitness[i] == best_fitness)
+			{
+				best_fitness_index_counter++;
+				best_fitness_index = i;
 			}
 		}
 
+		//check if soln found
+		if (best_fitness == 0 || best_fitness_index_counter == 10)
+		{
+			soln_found = true;
+		}
+
 		//tournament selection
-		
-		
 		for(int i = 0; i < 30; i++)
 		{
 			int k = randombytes_uniform(30);
 			int l = randombytes_uniform(30);
-			if(populationfitness[k] < populationfitness[l])
+			if(fabs(populationfitness[k]) < fabs(populationfitness[l]))
 			{
 				parent_1[i] = k;
 			}
@@ -83,10 +93,8 @@ int main(void)
 			}	
 
 			int m = randombytes_uniform(30);
-			printf("m:%d\n", m);
 			int n = randombytes_uniform(30);
-			printf("n:%d\n", n);
-			if(populationfitness[m] < populationfitness[n])
+			if(fabs(populationfitness[m]) < fabs(populationfitness[n]))
 			{
 				parent_2[i] = m;
 			}
@@ -95,18 +103,8 @@ int main(void)
 				parent_2[i] = n;
 			}	
 		}
-
-		for(int i =0 ; i< 30;i++)
-		{
-			printf("%d\n", parent_1[i]);
-		}
-			printf("parent 1 done\n");
-		for(int i =0 ; i< 30;i++)
-		{
-			printf("%d\n", parent_2[i]);
-		}
 		
-		//tried to get crossover going dont know if correct
+		//crossover
 		for(int i = 0; i < 30; i+=2)
 		{
 			for(int j = 0; j < 3; j++)
@@ -122,24 +120,41 @@ int main(void)
 			}
 		}
 		
-		return 0;
+		// mutation
+		for(int i = 0; i < 30; i++)
+		{
+			int random_gene = randombytes_uniform(6);
+			int random_mutation = (randombytes_uniform(10) - 5); /*random mutation between -5 and 5*/
+			new_population[i][random_gene] += random_mutation;
+		}
+
+		for(int i = 0; i < 30; i++)
+		{
+			for(int j = 0; j < 6; j++)
+			{
+				population[i][j] = new_population[i][j];
+			}
+		}
+
+		generation++;
+
+		
 	}
-}
 
-double double_rand_range(double a, double b)
-{
-	return (((((double)rand()) / ((double)(RAND_MAX))) * (a - b)) + b); /*generates random double between a(upper) and b(lower)*/
-}
-
-int int_rand_range(int a, int b)
-{
-	return ((((int)(rand()) / (int)(RAND_MAX)) * (a - b)) + b); /*generates random int between a(upper) and b(lower)*/
+	printf("%d\n", best_fitness_index);
+	printf("%lf\n", populationfitness[best_fitness_index]);
+	printf("generation: %d\n", generation-1);
+		
+	for(int i = 0; i< 6; i++)
+	{
+		printf("%d  ", best_fitness_soln[i]);
+	}
 }
 
 int cmpfunc(const void * a, const void * b) //compare function for qsort
 {
-	if (abs(*(double*)a) > abs(*(double*)b)) return 1;
-	else if (abs(*(double*)a) < abs(*(double*)b)) return -1;
+	if (fabs(*(double*)a) > fabs(*(double*)b)) return 1;
+	else if (fabs(*(double*)a) < fabs(*(double*)b)) return -1;
 	else return 0;
 }
 
